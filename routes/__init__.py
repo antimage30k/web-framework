332ef8cc93043fc814bf22c6fd07bbfb40e51a3d
RieRuuuu <930920259@qq.com>
2020-04-16 06:34:18 +0800

    fix note

--- routes/__init__.py
+++ routes/__init__.py
@@ -66,7 +66,7 @@ def error(request, code=404):
     根据 code 返回不同的错误响应
     目前只有 404
     """
-    # 之前上课我说过不要用数字来作为字典的 key
+    # 一般不要用数字来作为字典的 key
     # 但是在 HTTP 协议中 code 都是数字似乎更方便所以打破了这个原则
     e = {
         404: b'HTTP/1.x 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
