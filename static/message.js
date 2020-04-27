1b55a96619f6e74e93eb537a61e7cc8e6e44867c
RieRuuuu <930920259@qq.com>
2020-04-27 18:06:39 +0800

    fix notes

--- static/message.js
+++ static/message.js
@@ -4,35 +4,29 @@ var e = function (selector) {
     return document.querySelector(selector)
 }
 
-var todoTemplate = function (message_author, message_content) {
+var messageTemplate = function (message_author, message_content) {
     var t = `
-    <div class="todo-cell">
+    <div class="message-cell">
         <span>${message_author} 说：${message_content} </span>
     </div>
     `
-    /*
-    t = """
-    <div class="todo-cell">
-        <span>{}</span>
-    </div>
-    """.format(todo)
-     */
+
     return t
 }
 
 /*
 1. 给 add button 绑定事件
 2. 在事件处理函数中，获取 input 的值
-3. 用获取的值，组装一个 todo-cell HTML 字符串
-4. 插入 todo-list 中
+3. 用获取的值，组装一个 message-cell HTML 字符串
+4. 插入 message-list 中
 */
 
-var insertTodo = function (todoCell) {
-    var form = document.querySelector('#id-todo-list')
-    form.insertAdjacentHTML('beforeEnd', todoCell)
+var insertMessage = function (messageCell) {
+    var form = document.querySelector('#id-message-list')
+    form.insertAdjacentHTML('beforeend', messageCell)
 }
 
-var loadTodos = function () {
+var loadMessages = function () {
     ajax('POST', '/message/show', {}, function (json) {
         log('拿到ajax响应')
         log('response data', json)
@@ -40,9 +34,9 @@ var loadTodos = function () {
             log('json for', json[i], json)
             var message_author = json[i].username
             var message_content = json[i].content
-            var todoCell = todoTemplate(message_author, message_content)
-            log(todoCell)
-            insertTodo(todoCell)
+            var messageCell = messageTemplate(message_author, message_content)
+            log(messageCell)
+            insertMessage(messageCell)
         }
     })
 }
@@ -51,14 +45,14 @@ var bindEvents = function () {
     var b = e('#id-button-add')
     b.addEventListener('click', function () {
         log('click')
-        var input = e('#id-input-todo')
+        var input = e('#id-input-message')
         log(input)
         log(input.value)
         var message_content = input.value
         var author_name = e('#id-author-username')
         var author = author_name.value
-        var todoCell = todoTemplate(author, message_content)
-        log(todoCell)
+        var messageCell = messageTemplate(author, message_content)
+        log(messageCell)
 
         var data = {
             content: message_content
@@ -67,13 +61,13 @@ var bindEvents = function () {
             log('拿到ajax响应')
             var message = json.message
             alert(message)
-            insertTodo(todoCell)
+            insertMessage(messageCell)
         })
     })
 }
 
 var main = function () {
-    loadTodos()
+    loadMessages()
     bindEvents()
 }
 
